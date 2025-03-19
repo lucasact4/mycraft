3f957346f86c363318cea6447bc6ff0ee1655bfc
lucasact4 <lucasact4@outlook.com>
2025-03-18 22:36:38 -0300

    made several visual improvements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,37 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+
+document.addEventListener("turbo:load", function() {
+  const suns = document.querySelectorAll('.header-sun');
+
+  document.querySelector('.logo-mycraft').addEventListener('mouseenter', function() {
+      suns.forEach(function(sun) {
+          sun.style.top = '60px';
+          sun.style.left = '500px';
+      });
+  });
+
+  document.querySelector('.logo-mycraft').addEventListener('mouseleave', function() {
+      suns.forEach(function(sun) {
+          sun.style.top = '80px';
+          sun.style.left = '120px';
+      });
+  });
+});
+
+document.addEventListener("turbo:load", function() {
+  const checkbox = document.getElementById("menu-toggle");
+
+  function toggleCheckbox() {
+      if (window.innerWidth > 991) {
+          checkbox.disabled = true;
+          checkbox.checked = false;
+      } else {
+          checkbox.disabled = false;
+      }
+  }
+
+  toggleCheckbox();
+  window.addEventListener("resize", toggleCheckbox);
+});
